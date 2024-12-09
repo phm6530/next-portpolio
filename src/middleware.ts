b05a162caeb8a61ed1041464271ff889365c3ca6
unknown <phm>
2024-12-09 17:49:24 +0900

    gsap 배너 캐릭터 FOUC 방지

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,8 @@ enum PATH {
 export async function middleware(req: NextRequest, res: NextResponse) {
   const path = req.nextUrl.pathname;
   const qs = req.nextUrl.searchParams;
+
+
   const token = serverSession();
 
   //로그인 페이지인데 로그인 되어있을때 ReDirect 시켜버리기
