7bca8e3f9fdd0652fe2e20bcc943351304b5b50f
박현민 <squirrel309@naver.com>
2024-12-09 19:19:27 +0900

    [ModleToggle] Dark Mode 디자인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,10 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { BASE_NEST_URL } from "@/config/base";
-import fetchWithAuth from "@/utils/withRefreshToken";
 import { QUERY_KEY } from "@/types/constans";
-import DarkmodeHandler from "@/components/DarkModeHandler/DarkModeHandler";
+import fetchWithAuth from "@/utils/withRefreshToken";
 import ClientProvider from "@/provider/ClientProvider";
+import ModeToggle from "@/components/ModeToggle/ModeToggle";
 
 //메타 데이터
 export const metadata: Metadata = {
@@ -65,7 +65,7 @@ export default async function RootLayout({
               <GlobalNav />
               <main className="container">{children}</main>
               {/* Dark Mode handler */}
-              <DarkmodeHandler />
+              <ModeToggle />
             </HydrationBoundary>
           </ClientProvider>
         </ProviderContext>
