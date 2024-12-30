ab68dd3f160c95ef2e16ce22729d8c042714fa25
hm <squirrel309@naver.com>
2024-12-31 00:56:55 +0900

    [GlobalNav] Nav Active 스타일 + 반응형

---
+++ src/components/Header/NavLink.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import classes from "./NavLink.module.scss";
+import useStore from "@/store/store";
+
+export default function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  const store = useStore();
+
+  return (
+    <Link
+      href={href}
+      onClick={() => store.setClose()}
+      className={`${classes.link} ${isActive ? classes.active : undefined}`}
+    >
+      {children}
+    </Link>
+  );
+}
