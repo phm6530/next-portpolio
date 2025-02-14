2bf3fcf092cf6c24f5cb52732f249fa4edbe59b9
hm <squirrel309@naver.com>
2025-02-14 21:15:59 +0900

    revaildate 추상화

--- src/action/revaildate.ts
+++ src/action/revaildate.ts
@@ -2,7 +2,14 @@
 
 import { revalidateTag } from "next/cache";
 
-export const revaildateTags = async (tags: string[]) => {
-  console.log("초기화..");
+export const revaildateTags = async <T>(
+  cb: () => Promise<T>,
+  tags: string[]
+): Promise<T> => {
+  const result = await cb();
+
+  // 초기화..
   tags.forEach((tag) => revalidateTag(tag));
+
+  return result;
 };
